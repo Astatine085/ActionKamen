089eacc3c8991162553e154ab023057b9152a42a
Astatine 85 <118582815+Astatine085@users.noreply.github.com>
2022-11-25 16:06:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/arthur-dane/DoraemonPocketbot- /Eva
+  git clone https://github.com/Astatine085/ActionKamen /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
